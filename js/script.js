405df784739872a42d586ddbe88e8567d3aa798e
gfed53 <gfederico7153@gmail.com>
2015-11-14 17:27:41 -0500

    Added radio buttons for category page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-	
-})
+	$('.game-start').show();
+
+	//To go from start-game to choose-cat
+	$('.btn-start-game').on('click', function(){
+		$('.game-start').hide();
+		$('.game-choose-cat').show();
+	});
+
+	//Toggle buttons go to active when clicked
+	$('.btn-primary').on('click', function(){
+		$(this).button('toggle');
+	});
+	// $('.btn').on('click', function(){
+	// 	if($('.btn').hasClass('active')){
+	// 	} else{
+	// 		$(this).toggleClass('active');
+	// 	}
+	// });
+
+
+	//To go from choose-cat to game-question
+	$('.btn-select-cat').on('click', function(){
+		$('.game-choose-cat').hide();
+		$('.game-question').show();
+	});
+
+
+});
