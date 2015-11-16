d05e68afb15c42a3b7c7f5dc5fc26eddca83dc9d
gfed53 <gfederico7153@gmail.com>
2015-11-16 18:08:10 -0500

    Commit 6pm Monday

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var quizApp = {
 		numCorrect: 0,
 		numOfQuestions: 8,
-		// currentQ: 0,
+		currentQ: 0,
 		//Questions stored in array
 		questions: []
 	};
@@ -10,50 +10,67 @@ $(document).ready(function(){
 	//Constructor function for questions
 	function makeQuestion(title, answers, correct){
 		var question = {
-			title = title;
-			answers = answers;
-			correct = correct
+			title: title,
+			answers: answers,
+			correct: correct
 			// this.category = category;
 		}
+
 		quizApp.questions.push(question);
-		console.log(quizApp.questionsList);
-		console.log($('.question-1')[0];
+		console.log(quizApp.questions);
+		// console.log($('#question-1')[0]);
 	};
 
 	makeQuestion('First Answer', ['correct answer', 'wrong', 'wrong again', 'wrong wrong wrong'], 0);
 	makeQuestion('Second Answer', ['wrong', 'wrong', 'correct answer 2', 'wrong wrong wrong'], 2);
-
+	
 	
 
 	//Implant the questions 
-	// var questionInject = function(){
-	// 	quizApp.questions.forEach(function(q){
+	var questionInject = function(){
+		console.log($('.game-question label')[0]);
+		// quizApp.questions.forEach(function(q){
+		// 	console.log("Object");
 
-	// 		console.log("Object");
-	// 		if ($('.btn-group:nth-child')('active') === question.correct){
+			// if (q[0]) {
+			// 	$('.question-1').show();
+			// } else if (q[1])
+			// if ($(#)('active') === question.correct){
 				
 					
-	// 			};
-	// 		}
-	// 	});
-	// };
+			// 	};
+			// }
+		// });
+	};
+
+	questionInject();
 
 	//Detect whether selected choice is correct answer
 	var determineCorrect = function(){
-		console.log("Running determineCorrect");
-		var rightChoice = questionsList[records.currentQ];
-		console.log(rightChoice);
-		console.log(rightChoice.answer);
-		console.log($('.game-question.active span').html());
-		if($('.game-question .active span').html() === rightChoice.answer){
-			console.log("This should happen");
-			$('.answer-text h1').html(rightChoice.answer + " is correct!");
-			records.numCorrect += 1;
-			$('.num-correct').html(records.numCorrect);
+		var n = quizApp.questions[quizApp.currentQ].correct;
+		console.log(n);
+
+
+		if ($('.game-question label')[n].hasClass('active')){
+			console.log("You are correct");
 		} else {
-			$('.answer-text h1').html("Sorry, the answer is " + rightChoice.answer + ".");
+			console.log("You are wrong");
 		}
-		$('.answer-text p').html(rightChoice.blurb);
+
+		// console.log("Running determineCorrect");
+		// var rightChoice = questionsList[records.currentQ];
+		// console.log(rightChoice);
+		// console.log(rightChoice.answer);
+		// console.log($('.game-question.active span').html());
+		// if($('.game-question .active span').html() === rightChoice.answer){
+		// 	console.log("This should happen");
+		// 	$('.answer-text h1').html(rightChoice.answer + " is correct!");
+		// 	records.numCorrect += 1;
+		// 	$('.num-correct').html(records.numCorrect);
+		// } else {
+		// 	$('.answer-text h1').html("Sorry, the answer is " + rightChoice.answer + ".");
+		// }
+		// $('.answer-text p').html(rightChoice.blurb);
 	};
 
 	var clearRoundData = function(){
@@ -62,14 +79,14 @@ $(document).ready(function(){
 		//Or?
 		// $('.btn-primary').button('reset');
 		//Update score, question number, and question itself (make function?)
-		records.numAsked += 1;
-		$('.num-asked').html(records.numAsked);
-		records.currentQ += 1;
+		quizApp.numAsked += 1;
+		$('.num-asked').html(quizApp.numAsked);
+		quizApp.currentQ += 1;
 	};
 
 	//Check if all questions are asked. Goes in click function of select-answer?
 	var checkEnd = function(){
-		console.log(records.currentQ);
+		console.log(quizApp.currentQ);
 		console.log(questionsList.length-1);
 		if(records.currentQ === (questionsList.length)){
 			console.log("This is the end");
@@ -78,10 +95,10 @@ $(document).ready(function(){
 
 	$('.game-start').show();
 
-	//To go from start-game to choose-cat
+	//To go from start-game to question 1
 	$('.btn-start-game').on('click', function(){
 		$('.game-start').hide();
-		$('.game-choose-cat').show();
+		$('.question-1-section').show();
 	});
 
 	//Toggle buttons go to active when clicked
@@ -100,15 +117,15 @@ $(document).ready(function(){
 		
 	});
 
-	//Feedback
+	//Feedback(for question 1)
 	$('.btn-select-answer').on('click', function(){
 		determineCorrect();
 		//Shift phase
 		$('.game-question').hide();
 		$('#masthead').hide();
-		$('.game-answer').show();
+		$('.game-answer-1').show();
 		clearRoundData();
-		checkEnd();
+		// checkEnd();
 	});
 
 });
